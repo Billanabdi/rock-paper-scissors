e6bd59a0586c00a8c012699b4558544bfd15e381
Billan Abdi <billanabdi04@gmail.com>
2023-11-09 01:12:42 +0400

    Solution

--- challenge.js
+++ challenge.js
@@ -2,20 +2,15 @@
 // Write your code here.
 
 function playGame(player1, player2) {
-  if (player1 === "scissors" && player2 === "paper") {
-    return `player1 wins`;
-  } else if (player1 === "paper" && player2 === "scissors") {
-    return "player2 wins";
-  } else if (player1 === "rock" && player2 === "paper") {
-    return "player2 wins";
-  } else if (player1 === "paper" && player2 === "rock") {
+  if (player1 === player2) {
+    return "tie";
+  } else if (
+    (player1 === "rock" && player2 === "scissors") ||
+    (player1 === "scissors" && player2 === "paper") ||
+    (player1 === "paper" && player2 === "rock")
+  ) {
     return "player1 wins";
-  } else if (player1 === "rock" && player2 === "scissors") {
-    return "player1 wins";
-  } else if (player1 === "scissors" && player2 === "rock") {
+  } else {
     return "player2 wins";
-  } else player1 === player2;
-  return "It's a tie";
+  }
 }
-
-console.log(playGame("rock", "papers"));
